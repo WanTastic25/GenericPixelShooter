7796099a3796bc8c9a1886d3a30970f55e16f55d
Wan Ashraf Muzaffer <wanashraf2511@gmail.com>
2025-12-10 12:26:23 +0800

    Reworked knockback
    Let go of using Tween since its not great for physics and is more meant for animation
    
    Fixed invulnerability and removed a bunch of unused declared vars

--- first-game/Player/scripts/PlayerMovement.cs
+++ first-game/Player/scripts/PlayerMovement.cs
@@ -6,21 +6,20 @@ using static Godot.TextServer;
 public partial class PlayerMovement : CharacterBody2D
 {
     [Export] public float Speed = 100.0f;
-    [Export] public Boolean canDodge = true;
+    [Export] public Boolean canDash = true;
     [Export] public Boolean invulnerable = false;
-    [Export] public Boolean isDodging = false;
-    [Export] public float dodgeSpeed = 300.0f;
+    [Export] public Boolean isDashing = false;
 	//[Export] public PlayerStats stats;
 	[Export] public int playerHealth = 100;
-	private Timer dodgeAgainTimer;
+	private Timer dashAgainTimer;
 	private Timer invulnerabilityTimer;
 	private HealthBar healthBarCode;
-	private Vector2 knockbackVelocity;
-	[Export] private float knockbackDecay = 10f;
+	private double knockbackTimer;
+	private Vector2 knockback;
 
     public override void _Ready()
 	{
-        dodgeAgainTimer = GetNode<Timer>("dodgeAgainTimer");
+        dashAgainTimer = GetNode<Timer>("dashAgainTimer");
         invulnerabilityTimer = GetNode<Timer>("invulnerabilityTimer");
 		healthBarCode = GetNode<HealthBar>("healthBar");
 		healthBarCode.InitHealth(playerHealth);
@@ -28,30 +27,25 @@ public partial class PlayerMovement : CharacterBody2D
 
     public override void _PhysicsProcess(double delta)
 	{
-        Vector2 velocity = Velocity;
+        if (knockbackTimer > 0.00)
+        {
+            Velocity = knockback;
+            knockbackTimer -= delta;
 
-		Vector2 direction = Input.GetVector("left", "right", "up", "down");
-
-		if (direction != Vector2.Zero)
-			velocity = direction * Speed;
-
-		if (direction == Vector2.Zero)
-			velocity = Velocity.MoveToward(Vector2.Zero, Speed);
-
-		if (Input.IsActionPressed("dodge") && canDodge && !isDodging)
-		{	
-			_ = Dash(velocity, 150f, 0.08f);
-			canDodge = false;
-			dodgeAgainTimer.Start();
+            if (knockbackTimer <= 0.00)
+                knockback = Vector2.Zero;
+        }
+        else
+        {
+            movement();
         }
 
-		Velocity = velocity;
-		MoveAndSlide();
+            MoveAndSlide();
 	}
 
     public async Task Dash(Vector2 dashDirection, float distance, float duration)
     {
-		isDodging = true;
+		isDashing = true;
 		
         var tween = CreateTween();
         Vector2 start = GlobalPosition;
@@ -63,30 +57,46 @@ public partial class PlayerMovement : CharacterBody2D
 
         await ToSignal(tween, "finished");
 
-        isDodging = false;
+        isDashing = false;
 	}
 
-	public async Task knockback(Vector2 enemyDirection)
+	public void movement()
 	{
-        var tween = CreateTween();
-        Vector2 start = GlobalPosition;
-        Vector2 end = start + enemyDirection.Normalized() * 30;
+        Vector2 velocity = Velocity;
 
-        tween.TweenProperty(this, "global_position", end, 0.2f)
-             .SetTrans(Tween.TransitionType.Cubic)
-             .SetEase(Tween.EaseType.Out);
+        Vector2 direction = Input.GetVector("left", "right", "up", "down");
 
-        await ToSignal(tween, "finished");
+        if (direction != Vector2.Zero)
+            velocity = direction * Speed;
+
+        if (direction == Vector2.Zero)
+            velocity = Velocity.MoveToward(Vector2.Zero, Speed);
+
+        if (Input.IsActionPressed("dash") && canDash && !isDashing)
+        {
+            _ = Dash(velocity, 150f, 0.08f);
+            canDash = false;
+            dashAgainTimer.Start();
+        }
+
+        Velocity = velocity;
     }
 
-	public void invulnearble()
+	public void applyKnockback(Vector2 enemyDirection, float knockBackStrength, double knockbackDuration)
 	{
+        knockback = enemyDirection * knockBackStrength;
+		knockbackTimer = knockbackDuration;
+	}
+
+	public void applyInvulnerability()
+	{
+        invulnerable = true;
 		invulnerabilityTimer.Start();
     }
 
-	private void _on_dodge_again_timer_timeout()
+	private void _on_dash_again_timer_timeout()
 	{
-		canDodge = true;
+		canDash = true;
 	}
 
 	private void _on_invulnerability_timer_timeout()
